823b882da8649dc21bc6892316ddcf663bf5503c
John Adams <johnadams@jqatampa.com>
2015-01-13 12:26:31 -0500

    Refactoring some session jazz, supporting mongodb connection strings or environment vars

--- session/session.go
+++ session/session.go
@@ -1,9 +1,11 @@
 package session
 
 import (
+	"os"
+	"strings"
+
 	"bitbucket.org/tampajohn/gadget-arm/errors"
 	"gopkg.in/mgo.v2"
-	"os"
 )
 
 var (
@@ -12,9 +14,20 @@ var (
 
 func Get(connectionVariable string) *mgo.Session {
 	if mgoSession == nil {
-		cs := os.Getenv(connectionVariable)
+		var cs string
+
+		if strings.HasPrefix(connectionVariable, "mongodb://") {
+			cs = connectionVariable
+		} else {
+			cs = os.Getenv(connectionVariable)
+		}
+
 		var err error
 		mgoSession, err = mgo.Dial(cs)
+
+		// http://godoc.org/labix.org/v2/mgo#Session.SetMode
+		mgoSession.SetMode(mgo.Monotonic, true)
+
 		errors.Check(err)
 	}
 	return mgoSession.Clone()
