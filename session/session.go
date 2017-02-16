8c65c1cfb51ed1aeed1b3101c83fb60af95d6c3e
kiangj <justin@bringhub.com>
2017-02-16 13:51:55 -0800

    Update session.go
    Added SSL support

--- session/session.go
+++ session/session.go
@@ -6,14 +6,17 @@ import (
 	"strings"
 	"sync"
 
+	"crypto/tls"
+	"crypto/x509"
 	"github.com/jqatampa/gadget-arm/errors"
 	"gopkg.in/mgo.v2"
+	"net"
 )
 
 var sessions = make(map[string]*mgo.Session)
 var mutex = &sync.Mutex{}
 
-func Get(connectionVariable string) *mgo.Session {
+func Get(connectionVariable string, cert ...string) *mgo.Session {
 	if sessions[connectionVariable] == nil {
 		mutex.Lock()
 		if sessions[connectionVariable] == nil {
@@ -27,7 +30,12 @@ func Get(connectionVariable string) *mgo.Session {
 
 			var err error
 
-			session, err := mgo.Dial(cs)
+			var session *mgo.Session
+			if cert != nil {
+				session, err = dialWithSSL(cs, cert[0])
+			} else {
+				session, err = mgo.Dial(cs)
+			}
 
 			errors.Check(err)
 
@@ -44,4 +52,23 @@ func Get(connectionVariable string) *mgo.Session {
 	}
 
 	return sessions[connectionVariable].Copy()
+}
+
+func dialWithSSL(cs, pem string) (session *mgo.Session, err error) {
+	roots := x509.NewCertPool()
+	roots.AppendCertsFromPEM([]byte(pem))
+
+	tlsConfig := &tls.Config{}
+	tlsConfig.RootCAs = roots
+
+	dialInfo, err := mgo.ParseURL(cs)
+	if err != nil {
+		return
+	}
+	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+		conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
+		return conn, err
+	}
+	session, err = mgo.DialWithInfo(dialInfo)
+	return
 }
